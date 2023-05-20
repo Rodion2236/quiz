7e675779eabf73fd1922fc7c740bf040638e905a
Rodion <kosolapkinr@gmail.com>
2023-05-20 17:28:26 +0400

    commit

--- app/src/main/java/com/rodion2236/quiz/ui/MainActivity.kt
+++ app/src/main/java/com/rodion2236/quiz/ui/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.rodion2236.quiz.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.rodion2236.quiz.R
+import com.rodion2236.quiz.profile.CurrentProfile.Companion.instance
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentProfile = instance?.currentProfile
+
+        if (currentProfile != null) {
+            findNavController(R.id.nav_host_fragment).navigate(R.id.homeFragment)
+        } else {
+            findNavController(R.id.nav_host_fragment).navigate(R.id.signInFragment)
+        }
     }
 }
 
